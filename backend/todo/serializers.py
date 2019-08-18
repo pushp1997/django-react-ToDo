2de0cb1578573f79150e599527d87700a5a9a853
Pushp Vashisht <pushptyagi1@gmail.com>
2019-08-19 02:18:27 +0530

    Creating serializers for the Todo model

---
+++ backend/todo/serializers.py
@@ -0,0 +1,9 @@
+# We need serializers to convert model instances to JSON so that
+# the frontend can work with the received data easily.
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        mdoel = Todo
+        fields = ('id', 'title', 'description', 'completed')
